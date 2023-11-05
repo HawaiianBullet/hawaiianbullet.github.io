9793fd8f9fe7957fea87f55bde9cfc8b91fbd5bf
sskim950105 <sskim950105@gmail.com>
2023-11-05 10:27:44 +0900

    feat: add blog layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@nuxt/content']
-})
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+  content: {
+    highlight: {
+      theme: {
+        default: "material-theme-palenight",
+        dark: "github-dark",
+      },
+    },
+    markdown: {
+      toc: {
+        depth: 5,
+        searchDepth: 5,
+      },
+    },
+  },
+  tailwindcss: {
+    cssPath: "~/assets/css/main.css",
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <section class="hero-section">
+      <div class="wrapper">
+        <header>
+          <h1 class="font-black text-7xl">SSK 블로그입니다.</h1>
+          <p>블로그로 <NuxtLink to="/blog">이동</NuxtLink></p>
+        </header>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+.hero-section {
+  @apply flex justify-center p-4 w-full h-screen bg-slate-50 text-slate-700;
+}
+
+.hero-section > .wrapper {
+  @apply m-auto max-w-6xl;
+}
+</style>
